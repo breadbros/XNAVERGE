654e393002599002971d3f3cf260925fd9d78762
Gayo <halfassured@gmail.com>
2012-03-20 02:16:08 -0300

    Fixed followers "wobbling" at some speeds.

--- XNAVERGE/sprite/FollowerChain.cs
+++ XNAVERGE/sprite/FollowerChain.cs
@@ -9,8 +9,7 @@ namespace XNAVERGE {
     //    Represents a chain of entities trailing behind the player. Those entities will not be updated 
     // normally (their movement and obstruction handling isn't even called, so they can neither be
     // obstructed nor obstruct others). They mimic the player's movements exactly, onward down the 
-    // chain, even when player_controllable is false. If the player is in the list of followers, it will 
-    // simply be treated as though it weren't.
+    // chain, even when player_controllable is false. 
     //    The first entity in the list follows the player, the second follows that entity, and so on. 
     public class FollowerChain {
         public Entity leader { get; private set; }
@@ -39,87 +38,86 @@ namespace XNAVERGE {
         }
 
         public void Update() {
-            FollowerData cur;
             Vector2 exact_pos;
-            Point old_pos, new_pos;            
-            int distance, goal_dist, num_followers = list.Count;
+            Point old_pos, new_pos, move_step;
+            Direction dir;
+            int distance, goal_dist, cur_idx, num_followers = list.Count;
             if (num_followers == 0) return;
             exact_pos = leader.exact_pos - prev_leader_position;
 
             old_pos = new Point((int)prev_leader_position.X, (int)prev_leader_position.Y);
             new_pos = leader.hitbox.Location;
-            
             new_pos.X -= old_pos.X;
             new_pos.Y -= old_pos.Y;
-
-            distance = Math.Abs(new_pos.X) + Math.Abs(new_pos.Y);
-
-            if (distance > 0) {
+            Console.WriteLine("!!!");
+            dir = Utility.direction_from_signs(Math.Sign(exact_pos.X), Math.Sign(exact_pos.Y), false);            
+            while (new_pos != Point.Zero) {
                 _inc_first();
-                path_buffer[first].offset = new_pos;
-                path_buffer[first].direction = Utility.direction_from_signs(Math.Sign(exact_pos.X), Math.Sign(exact_pos.Y), false);
+                move_step = new Point(Math.Sign(new_pos.X), Math.Sign(new_pos.Y));                
+                old_pos.X += move_step.X;
+                old_pos.Y += move_step.Y;
+                new_pos.X -= move_step.X;
+                new_pos.Y -= move_step.Y;
+                path_buffer[first].position = old_pos;
+                path_buffer[first].distance = Math.Abs(move_step.X) + Math.Abs(move_step.Y);
+                path_buffer[first].direction = dir;
             }
 
-            for (int i = 0; i < num_followers; i++) {                
-                goal_dist = (1+i)*step;
-                cur = list[i];
-                old_pos = cur.entity.hitbox.Location;
-                cur.dist += distance;                
-                while (cur.dist > goal_dist && cur.idx != first) {
-                    cur.entity.last_logic_tick = VERGEGame.game.tick;                    
-                    if (i == (num_followers - 1)) { // bringing up the rear
-                        _inc_last();
-                        cur.idx = last;
-                    }
-                    else cur.idx = _next_idx(cur.idx);                    
-                    new_pos = path_buffer[cur.idx].offset;
-                    cur.entity.x += new_pos.X;
-                    cur.entity.y += new_pos.Y;
-                    cur.dist -= Math.Abs(new_pos.X) + Math.Abs(new_pos.Y);                    
+            cur_idx = first;
+            distance = 0;
+            for (int i = 0; i < num_followers; i++) {
+                goal_dist = (i+1)*step;
+                old_pos = list[i].entity.hitbox.Location;
+                while (distance < goal_dist) {                                        
+                    distance += path_buffer[cur_idx].distance;
+                    cur_idx = _next_idx(cur_idx);
+                    if (cur_idx == last) distance = Int32.MaxValue;
                 }
-
-                new_pos = cur.entity.hitbox.Location;
+                new_pos = path_buffer[cur_idx].position;
                 if (old_pos == new_pos) {
-                    if (cur.entity.moving && !leader.moving) cur.entity.set_walk_state(false);
+                    if (list[i].entity.moving && !leader.moving) list[i].entity.set_walk_state(false);
                 }
                 else {
-                    if (!cur.entity.moving) cur.entity.set_walk_state(true);
-                    cur.entity.facing = path_buffer[cur.idx].direction;
-                }
-
+                    list[i].entity.x = new_pos.X;
+                    list[i].entity.y = new_pos.Y;
+                    if (!list[i].entity.moving) list[i].entity.set_walk_state(true);
+                    list[i].entity.facing = path_buffer[cur_idx].direction;
+                }                
             }
+            last = cur_idx;
+            
         }
 
         protected void reset_buffer() {
             if (list.Count == 0) path_buffer = new MovementData[1];
             else {
-                path_buffer = new MovementData[list.Count * step + 2];
-                foreach (FollowerData d in list) {
-                    d.idx = 0;
-                    d.dist = Math.Abs(leader.x - d.entity.x) + Math.Abs(leader.y - d.entity.y);
-                }
+                path_buffer = new MovementData[list.Count * step + 1];
             }
             first = 0;
             last = 0;
+            path_buffer[0].position = leader.hitbox.Location;
         }
 
         protected void _inc_first() {            
             first++;
-            if (first >= path_buffer.Length) first = 0;
-            System.Diagnostics.Debug.Assert(first != last);
-        }
-
-        protected void _inc_last() {
-            if (last != first) {
+            if (first >= path_buffer.Length) first = 0;            
+            if (first == last) {
                 last++;
                 if (last >= path_buffer.Length) last = 0;
             }
         }
 
-        protected int _next_idx(int idx) {            
+        protected int _prev_idx(int idx) {
             if (idx == first) return idx; // can't go any farther
             idx++;
             if (idx >= path_buffer.Length) return 0;
+            return idx;
+        }
+
+        protected int _next_idx(int idx) {            
+            if (idx == last) return idx; // can't go any farther
+            idx--;
+            if (idx < 0) return (path_buffer.Length - 1);
             return idx;
         }
 
@@ -165,16 +163,18 @@ namespace XNAVERGE {
         }
 
         protected struct MovementData {
-            public Point offset;
+            public Point position;
             public Direction direction; // may be different than offset implies, due to subpixel movement
+            public int distance;
         }
 
         protected class FollowerData {
             public Entity entity;
-            public int dist;
-            public int idx;
-
-            public FollowerData(Entity e) { entity = e; }
+            public int original_speed;
+            public FollowerData(Entity e) { 
+                entity = e;
+                original_speed = entity.speed;
+            }
         }
 
     }
