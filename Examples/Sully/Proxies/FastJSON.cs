7555d3d2dae3c2ae33ab198ebc2e6d42d27192ec
Kevin Gadd <kevin.gadd@gmail.com>
2012-04-11 12:45:37 -0700

    Add proxies for JSIL

---
+++ Examples/Sully/Proxies/FastJSON.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSIL.Meta;
+using JSIL.Proxy;
+using fastJSON;
+
+namespace Sully.Proxies {
+    [JSProxy(
+        typeof(fastJSON.JSON),
+        memberPolicy: JSProxyMemberPolicy.ReplaceDeclared,
+        attributePolicy: JSProxyAttributePolicy.ReplaceDeclared
+    )]
+    public class FastJSONProxy {
+        [JSReplacement("JSIL.JSON.Parse($json)")]
+        [JSIsPure]
+        public object Parse (string json) {
+            throw new InvalidOperationException();
+        }
+    }
+}
